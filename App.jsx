12defe0e39c8bdd25cecaaf95de247e071aae066
Alejandro Sanchez Pabon <andalemono@gmail.com>
2026-05-12 22:28:30 -0500

    Delete App.jsx

--- App.jsx
+++
@@ -1,9 +0,0 @@
-{
-  "short_name": "Dekmantel",
-  "name": "Road to Dekmantel - Fitness Tracker",
-  "start_url": ".",
-  "display": "standalone",
-  "theme_color": "#E8553A",
-  "background_color": "#F6F5F1",
-  "icons": []
-}
